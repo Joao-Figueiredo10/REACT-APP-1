3af0b8545d46c4fb1fa6c5c9286fd16460ee5678
Joao-Figueiredo10 <a14871@oficina.pt>
2026-05-19 09:44:27 +0000

    commitada do mal

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css"
+import heroImg from './assets/hero.png'
+
+function Header(){
+    return(
+        <>
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+
+        </>
+    )
+}
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Message from './Message.jsx'
+import Header from './Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
+    <Message/>
+    <App></App>
   </StrictMode>,
 )
+
+
