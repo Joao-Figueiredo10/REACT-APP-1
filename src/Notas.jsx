bef78b28fa5ca70b91b6cead5c5c42ac67d8269c
Joao-Figueiredo10 <a14871@oficina.pt>
2026-05-25 09:34:23 +0000

    commit

--- src/Notas.jsx
+++ src/Notas.jsx
@@ -23,45 +23,50 @@ function Notas() {
                     <input type="text" class="form-control" id="nome" placeholder="Insira o nome do aluno" value={formData.nome} onChange={(e) =>
                         setFormData({ ...formData, nome: e.target.value })} required />
                 </div>
-                <div class="form-group">
-                    <label for="notaTeste">Nota do teste</label>
-                    <input type="number" class="form-control" id="notaTeste" placeholder="Insira a nota do teste" value={formData.notaTeste} onChange={(e) =>
-                        setFormData({ ...formData, notaTeste: e.target.value })} required />
-                </div>
-                <div class="form-group">
-                    <label for="teste">Descrição do teste</label>
-                    <input type="text" class="form-control" id="teste" placeholder="Insira a descrição do teste" value={formData.teste} onChange={(e) =>
-                        setFormData({ ...formData, teste: e.target.value })} required />
-                </div>
-                <div class="form-group">
-                    <label for="notaTrabalho">Nota do trabalho</label>
-                    <input type="number" class="form-control" id="notaTrabalho" placeholder="Insira a nota do trabalho" value={formData.notaTrabalho} onChange={(e) =>
-                        setFormData({ ...formData, notaTrabalho: e.target.value })} required />
-                </div>
-                <div class="form-group">
-                    <label for="trabalho">Descrição do trabalho</label>
-                    <input type="text" class="form-control" id="trabalho" placeholder="Insira a descrição do trabalho" value={formData.trabalho} onChange={(e) =>
-                        setFormData({ ...formData, trabalho: e.target.value })} required />
-                </div>
-                <div class="form-group">
-                    <label for="notaAtitude">Nota da atitude</label>
-                    <input type="number" class="form-control" id="notaAtitude" placeholder="Insira a nota da atitude" value={formData.notaAtitude} onChange={(e) =>
-                        setFormData({ ...formData, notaAtitude: e.target.value })} required />
-                </div>
-                <div class="form-group">
-                    <label for="atitude">Descrição da atitude</label>
-                    <input type="text" class="form-control" id="atitude" placeholder="Insira a descrição da atitude" value={formData.atitude} onChange={(e) =>
-                        setFormData({ ...formData, atitude: e.target.value })} required />
+
+                <div class="row">
+                    <div class="col">
+                        <label for="notaTeste">Nota dos testes</label>
+                        <input type="number" class="form-control" id="notaTeste" placeholder="Insira a nota do teste" value={formData.notaTeste} onChange={(e) =>
+                            setFormData({ ...formData, notaTeste: e.target.value })} required />
+                    </div>
+
+
+
+                    <div class="col">
+                        <label for="teste">(% do teste)</label>
+                        <input type="text" class="form-control" id="teste" placeholder="Insira o percentual do teste" value={formData.teste} onChange={(e) =>
+                            setFormData({ ...formData, teste: e.target.value })} required /></div>
+
+                    <div class="w-100"></div>
+
+                    <div class="col">
+                        <label for="notaTrabalho">Nota dos trabalhos</label>
+                        <input type="number" class="form-control" id="notaTrabalho" placeholder="Insira a nota do trabalho" value={formData.notaTrabalho} onChange={(e) =>
+                            setFormData({ ...formData, notaTrabalho: e.target.value })} required />
+                    </div>
+
+                    <div class="col">
+                        <label for="trabalho">(%) trabalhos</label>
+                        <input type="text" class="form-control" id="trabalho" placeholder="Insira o percentual dos trabalhos" value={formData.trabalho} onChange={(e) =>
+                            setFormData({ ...formData, trabalho: e.target.value })} required />
+                    </div>
+
+                    <div class="w-100"></div>
+
+                    <div class="col">
+                        <label for="notaAtitude">Nota da atitude</label>
+                        <input type="number" class="form-control" id="notaAtitude" placeholder="Insira a nota da atitude" value={formData.notaAtitude} onChange={(e) =>
+                            setFormData({ ...formData, notaAtitude: e.target.value })} required />
+                    </div>
+
+                    <div class="col">
+                        <label for="atitude">(% da atitude)</label>
+                        <input type="text" class="form-control" id="atitude" placeholder="Insira o percentual da atitude" value={formData.atitude} onChange={(e) =>
+                            setFormData({ ...formData, atitude: e.target.value })} required />
+                    </div>
                 </div>
             </form>
-            <div class="row">
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="w-100"></div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-            </div>
-            
         </div>
     );
 }
